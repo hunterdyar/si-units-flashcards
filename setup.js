612e189925d5160c77ed8cc1cf35b534183ff274
Hunter Dyar <bloop@hdyar.com>
2025-09-22 14:11:38 -0400

    Initial commit

---
+++ setup.js
@@ -0,0 +1,73 @@
+
+flashcards.openDeck('101');
+if (!flashcards.deckLength()) {
+  flashcards.setDisplayName('Base Unit Abbreviations');
+  flashcards.addCards(
+    ['s', 'second'],
+    ['m', 'meter '],
+    ['kg', 'kilogram'],
+    ['A', 'ampere'],
+    ['K', 'kelvin'],
+    ['cd', 'candela'])
+}
+
+
+flashcards.openDeck('101');
+if (!flashcards.deckLength()) {
+  flashcards.setDisplayName('Base Unit is Quantity of What');
+  flashcards.addCards(
+      ['second', 'time'],
+      ['meter', 'length'],
+      ['kilogram', 'mass'],
+      ['ampere', 'electric current'],
+      ['kelvin', 'temperature'],
+      ['candela', 'luminous intensity'])
+}
+
+flashcards.openDeck('102');
+if (!flashcards.deckLength()) {
+  flashcards.setDisplayName('Prefixes To Decimal');
+  flashcards.addCards(
+      ['deca', '10'],
+      ['deci', '.1'],
+      ['hecto', '100'],
+      ['kilo', '1000'],
+      ['giga', '1000000000'],
+      ['centi', '.01'],
+      ['milli', '.001'],
+      ['micro', '.000001'],
+      ['nano', '.000000001'],
+      ['mega', '1000000'])
+}
+
+flashcards.openDeck('104');
+if (!flashcards.deckLength()) {
+  flashcards.setDisplayName('Decimal To Prefixes');
+  flashcards.addCards(
+      ['10', 'deca'],
+      ['.1', 'deci'],
+      ['100', 'hecto'],
+      ['1000', 'kilo'],
+      ['1000000000', 'giga'],
+      ['.01', 'centi'],
+      ['.001', 'milli'],
+      ['.000001', 'micro'],
+      ['.000000001', 'nano'],
+      ['1000000', 'mega'])
+}
+
+
+flashcards.openDeck('200');
+if (!flashcards.deckLength()) {
+  flashcards.setDisplayName('Derived Units');
+  flashcards.addCards(
+      ['Hz', 'frequency'],
+      ['radian', 'angle'],
+      ['newton', 'force'],
+      ['joule', 'energy'],
+      ['watt', 'power'],
+      ['coulomb', 'charge'],
+      ['coulomb', 'charge'],
+      ['volt', 'voltage'])
+}
+
